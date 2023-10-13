ad11b93aedbf6e908391fbf6dd9f60b613120296
SantiBcra <santiariel1705@gmail.com>
2023-10-13 16:32:24 +0100

    realizado el review completo

--- back/src/controllers/review/getReviews.js
+++ back/src/controllers/review/getReviews.js
@@ -1,20 +1,23 @@
-const { Review } = require("../../db");
+const { Review, } = require("../../db");
 
 const getReviews = async () => {
   try {
-    const allReviews = await Review.findAll({
-      attributes: ["id", "rate", "description"],
-    });
+    const allReviews = await Review.findAll(
+      // {
+      //  attributes: ["id", "rate", "description"],
+      // }
+      );
 
-    const reviewFormat = allReviews.map((review) => {
-      return {
-        id: review.dataValues.id,
-        rate: review.dataValues.rate,
-        description: review.dataValues.description,
-      };
-    });
+    // const reviewFormat = allReviews.map((review) => {
+    //   return {
+    //     id: review.dataValues.id,
+    //     rate: review.dataValues.rate,
+    //     description: review.dataValues.description,
+    //     FieldId:review.dataValues.Fieldid
+    //   };
+    // });
 
-    return reviewFormat;
+    return allReviews;
   } catch (error) {
     console.error("Error al buscar reseñas:", error);
     return null;
--- back/src/controllers/review/postReviews.js
+++ back/src/controllers/review/postReviews.js
@@ -1,17 +1,36 @@
-const { Review } = require("../../db");
+const { Review, Field, User } = require("../../db");
 
-const postReviews = async (rate, description) => {
+const postReviews = async (rate, description, FieldId, UserId) => {
   try {
     if (!rate || !description) {
       throw new Error("Faltan datos por completar");
     }
 
+    const user = await User.findOne({ where: { id: UserId } });
+    const field = await Field.findOne({ where: { id: FieldId } });
+  
+    if (!field) {
+      throw new Error("Cancha no encontrada");
+  }
+
     const review = await Review.create({
       rate,
       description,
+      FieldId : field.id,
+      UserId : user.id
+      
     });
 
-    return review;
+    const fieldWithReview = await Field.findByPk(field.id);
+    const userdWithReview = await User.findByPk(user.id);
+
+    return {
+      review: review,
+      cancha: fieldWithReview ,
+      user: userdWithReview,
+  };
+    
+    
   } catch (error) {
     console.error(error);
     throw new Error(error.message);
