d6d809e6495ae2798f503440f0e0e3f29de97421
ProyectoZucca <proyectozucca@gmail.com>
2023-10-17 16:58:06 -0300

    error usestate

--- front/src/components/Home/Home.jsx
+++ front/src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 //import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
+import {useState} from 'react';
 import { NavBar } from "../index"
 import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
