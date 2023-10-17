fd9ab6fcb628c838cd8b5a50c031984549330ae1
Emiliano006 <emiliano.costabile@hotmail.com>
2023-10-16 21:40:42 -0300

    css home

--- front/src/components/NavBar/NavBar.jsx
+++ front/src/components/NavBar/NavBar.jsx
@@ -8,20 +8,39 @@ const NavBar = () => {
   return (
     <div className={style.homeContainer}>
       <div className={style.navbar}>
-        <h1>Navbar</h1>
-      </div>
-      {isHomePage ? null : (
-        <div>
-          <Link to={'/login'}><button className={style.btn}>Iniciar Sesión</button></Link>
-          <Link to={'/signup'}><button className={style.btn}>Registrarse</button></Link>
+        <div className={style.logo}>
+          <Link to={"/home"} style={{ font: '28px Poppins, sans-serif' }}>Canchas Ya</Link>
         </div>
-      )}
+      {/* {isHomePage ? null : (
+        <div>
+        <Link to={'/login'}><button className={style.btn}>Iniciar Sesión</button></Link>
+        <Link to={'/signup'}><button className={style.btn}>Registrarse</button></Link>
+        </div>
+      )} */}
+      <div className={style.btnContainer}>
       <Link to={'/form'}><button className={style.btn}>Agregar Cancha</button></Link>
+      </div>
+      </div>
+      <div className={style.text}>
+        <div className={style.texto}>
+        <h1 className={style.verde}>NO TE QUEDES SIN JUGAR</h1>
+        <h1>ENCUENTRA LA CANCHA MAS CERCANA A TI</h1>
+        </div>
+      </div>
+      <div className={style.nav}>
+        <div className={style.filtros}>
+        <button>Ciudad</button>
+        <button>Deporte</button>
+        <button>Fecha</button>
+        <button>Hora</button>
+        </div>
       <div className={style.search}>
-        <input type="text" placeholder="Buscar..." />
+        <input type="text" placeholder="Buscar cancha por nombre" />
+      </div>
       </div>
     </div>
   )
 }
 
 export default NavBar
+
