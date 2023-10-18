c62b4b2dff131fcf097502f0317cb6cf78a4f1d5
unknown <joaquinjavier.naya@gmail.com>
2023-10-18 15:36:30 -0300

    sposrts

--- back/src/controllers/sports/getSports.js
+++ back/src/controllers/sports/getSports.js
@@ -1,25 +1,16 @@
 const { Sport } = require("../../db");
 
 const getAllSports = async () => {
-  const sports = [
-    "Fútbol",
-    "Básquet",
-    "Rugby",
-    "Tenis",
-    "Vóley",
-    "Boxeo",
-    "Hockey",
-  ];
-  sports.forEach(s =>{
-    if (s){
-        Sport.findOrCreate({
-            where: {name : s}
-        })
+  const sports = ["Fútbol", "Básquet", "Padel", "Tenis", "Vóley"];
+  sports.forEach((s) => {
+    if (s) {
+      Sport.findOrCreate({
+        where: { name: s },
+      });
     }
-});
+  });
 
-
-const deportes = Sport.findAll()
+  const deportes = Sport.findAll();
 
   return deportes;
 };
