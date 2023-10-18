810135121db8f763e587203ded49a942ce4f82c5
unknown <joaquinjavier.naya@gmail.com>
2023-10-18 15:55:03 -0300

    mockupsssss

--- mockUp.js
+++ mockUp.js
@@ -3,301 +3,344 @@
     name: "Fútbol Cancha",
     city: "Buenos Aires",
     price: "10000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Fútbol",
   },
   {
     name: "F5 Canchas",
     city: "Buenos Aires",
     price: "15000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Fútbol",
   },
   {
     name: "Canchitas F5",
     city: "Buenos Aires",
     price: "12000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Fútbol",
   },
   {
     name: "Buenos Aires Fútbol",
     city: "Buenos Aires",
     price: "11000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Fútbol",
   },
   {
     name: "Complejo Básquet",
     city: "Buenos Aires",
     price: "13000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Básquet",
   },
   {
     name: "Fútbol por Hora",
     city: "Buenos Aires",
     price: "10000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Fútbol",
   },
   {
     name: "Cancha 2",
     city: "Buenos Aires",
     price: "11500",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Fútbol",
   },
   {
     name: "Canchas Vóley",
     city: "Buenos Aires",
     price: "12000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Vóley",
   },
   {
     name: "Fútbol Camp",
     city: "Buenos Aires",
     price: "9000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Fútbol",
   },
   {
     name: "Cancha San Martín",
     city: "Buenos Aires",
     price: "18000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Padel",
   },
   {
     name: "Belgrano Fútbol",
     city: "Buenos Aires",
     price: "19000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Fútbol",
   },
   {
     name: "Saavedra Básquet",
     city: "Buenos Aires",
     price: "11000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Básquet",
   },
   {
     name: "Mendoza Padel",
     city: "Buenos Aires",
     price: "14000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Padel",
   },
   {
     name: "San Cristóbal",
     city: "Buenos Aires",
     price: "8000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Tenis",
   },
   {
     name: "Dorrego Fútbol",
     city: "Buenos Aires",
     price: "9000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Fútbol",
   },
   {
     name: "San Martín",
     city: "Rosario",
     price: "7000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Tenis",
   },
   {
     name: "Quique Fútbol",
     city: "Rosario",
     price: "6000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Fútbol",
   },
   {
     name: "Romario Futebol",
     city: "Rosario",
     price: "17400",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Fútbol",
   },
   {
     name: "Chapu Canchas",
     city: "Rosario",
     price: "14030",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Vóley",
   },
   {
     name: "Blanco Encalada Básquet",
     city: "Rosario",
     price: "4500",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Básquet",
   },
   {
     name: "San Lorenzo de Rosario",
     city: "Rosario",
     price: "13000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Básquet",
   },
   {
     name: "Constitución",
     city: "Rosario",
     price: "10000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Vóley",
   },
   {
     name: "Messi F5",
     city: "Rosario",
     price: "10000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Fútbol",
   },
   {
     name: "Di María F5",
     city: "Rosario",
     price: "11000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Fútbol",
   },
   {
     name: "Vóley Rosario",
     city: "Rosario",
     price: "9000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Vóley",
   },
   {
     name: "Rosario Sur",
     city: "Rosario",
     price: "6000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Padel",
   },
   {
     name: "Rosario Norte",
     city: "Rosario",
     price: "12000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Tenis",
   },
   {
     name: "F5 Ahora",
     city: "Rosario",
     price: "10000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Fútbol",
   },
   {
     name: "Talleres de Córdoba",
     city: "Córdoba",
     price: "15000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Fútbol",
   },
   {
     name: "Guemes Padel",
     city: "Córdoba",
     price: "8000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Padel",
   },
   {
     name: "San Girona",
     city: "Córdoba",
     price: "13000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Padel",
   },
   {
     name: "Solís Fútbol",
     city: "Córdoba",
     price: "12000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Fútbol",
   },
   {
     name: "Pueyrredón Básquet",
     city: "Córdoba",
     price: "13000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Básquet",
   },
   {
     name: "Chacabuco Fútbol",
     city: "Córdoba",
     price: "14000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Fútbol",
   },
   {
     name: "Federico Lacroze",
     city: "Córdoba",
     price: "9000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Tenis",
   },
   {
     name: "Sol de Mayo",
     city: "Córdoba",
     price: "16000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Básquet",
   },
   {
     name: "Club Parque",
     city: "Córdoba",
     price: "11000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Vóley",
   },
   {
     name: "Cullen Padel",
     city: "Córdoba",
     price: "9000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Padel",
   },
   {
     name: "Crespo Fútbol",
     city: "Córdoba",
     price: "4300",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Fútbol",
   },
   {
     name: "Lo de Pepe Fútbol",
     city: "Córdoba",
     price: "8000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Fútbol",
   },
   {
     name: "Cancha 3",
     city: "Córdoba",
     price: "11600",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Tenis",
   },
   {
     name: "Vóley Pueyrredón",
     city: "Córdoba",
     price: "6000",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Vóley",
   },
   {
     name: "Silvera Fútbol",
     city: "Córdoba",
     price: "4600",
-    image: "https://images2.alphacoders.com/121/1214052.png",
+    image:
+      "https://www.neuqueninforma.gob.ar/wp-content/uploads/2022/10/web-Cancha-de-cesped-sintetico-El-Alamito-46.jpg",
     Sports: "Fútbol",
   },
 ];
