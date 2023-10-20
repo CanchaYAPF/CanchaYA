cbb4791f7e6264c2812d71a27425552e07ba6ec4
unknown <joaquinjavier.naya@gmail.com>
2023-10-20 16:15:58 -0300

    mercado

--- front/src/components/Details/Details.jsx
+++ front/src/components/Details/Details.jsx
@@ -6,6 +6,26 @@ import style from './Details.module.css';
 /* import NavBar from '../NavBar/NavBar'; */
 
 function Details() {
+  const handlePaymentMercado = () => {
+
+    const paymentData = {
+      id: field.id, 
+      items:1,
+      title: field.name, 
+      description: `Reserva de la cancha ${field.name}`, 
+      image: field.image, 
+      price: field.price, 
+    };
+
+    console.log(paymentData);
+
+  axios
+    .post("http://localhost:3001/payment/createOrder", paymentData)
+    .then((response) => {
+      window.location.href = response.data.body.init_point;
+    })
+    .catch((error) => console.log(error.message));
+};
   const { id } = useParams();
   const dispatch = useDispatch();
 
@@ -33,7 +53,7 @@ function Details() {
       <h2>Precio: {field.price}</h2>
       <h2>Metodos de Pago: {field.paymentMethod}</h2>
 
-
+      <button onClick={handlePaymentMercado}>Pagar ahora con mercado pago</button>
       </div>
     </div>
       </div>
