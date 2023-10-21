dddd14a08103447d645b2a5ce2a59991775a8140
Emiliano006 <emiliano.costabile@hotmail.com>
2023-10-21 20:39:39 -0300

    Navbar acomodada

--- front/src/components/NavBar/NavBar.jsx
+++ front/src/components/NavBar/NavBar.jsx
@@ -19,48 +19,49 @@ const NavBar = ({ handleSearchChange }) => {
   const isLoginOrSignup =
     location.pathname === '/login' || location.pathname === '/signup';
 
-  return (
-    <div className={style.homeContainer}>
-      {!isLoginOrSignup && (
-        <div className={style.navbar}>
-          <div className={style.logo}>
-            <Link to="/home" style={{ font: '28px Poppins, sans-serif' }}>
-              <img src={logo} alt="" />
-            </Link>
-          </div>
-          <div className={style.search}>
-            <input
-              type="search"
-              placeholder="Buscar cancha por nombre"
-              onChange={handleSearchChange}
-            />
-          </div>
-          <div className={style.btnContainer}>
-            <Link to="/form">
-              <button className={style.btn}>Agregar Cancha</button>
-            </Link>
-          </div>
-          <div className={style.profileButton}>
-            <button className={style.btn} onClick={toggleProfileMenu}>
-              Mi Perfil
-            </button>
-            {isProfileMenuOpen && (
-              <div className={style.profileMenu}>
-                <Link to="/Profile">Ver Mi Perfil</Link>
-                <button onClick={logoutFunction}>Cerrar Sesión</button>
-              </div>
-            )}
-          </div>
+    return (
+      <div className={style.homeContainer}>
+        {!isLoginOrSignup && (
+          <div className={style.navbar}>
+            <div className={style.logo}>
+              <Link to="/home" style={{ font: '28px Poppins, sans-serif' }}>
+                <img src={logo} alt="" />
+              </Link>
+            </div>
+            <div className={style.search}>
+              <input
+                type="search"
+                placeholder="Buscar cancha por nombre"
+                onChange={handleSearchChange}
+              />
+            </div>
             <div className={style.text}>
               <div className={style.texto}>
                 <h1 className={style.verde}>NO TE QUEDES SIN JUGAR</h1>
                 <h1 className={style.blanco}>ENCUENTRA LA CANCHA MAS CERCANA A TI</h1>
               </div>
-        </div>
             </div>
-      )}
-    </div>
-  );
+            <div className={style.btnContainer}>
+              <Link to="/form">
+                <button className={style.btn}>Agregar Cancha</button>
+              </Link>
+            </div>
+            <div className={style.profileButton}>
+              <button className={style.btn} onClick={toggleProfileMenu}>
+                Mi Perfil
+              </button>
+              {isProfileMenuOpen && (
+                <div className={style.profileMenu}>
+                  <Link to="/Profile">Ver Mi Perfil</Link>
+                  <button onClick={logoutFunction}>Cerrar Sesión</button>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+    );
+    
 };
 
 export default NavBar;
