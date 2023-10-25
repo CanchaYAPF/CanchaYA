01c38ab4859f7322cfe255a4e9c22bceeaa2d23e
vapretito <titomanbarreto@gmail.com>
2023-10-25 13:30:02 -0300

    booking userid / login boton

--- front/src/components/Booking/Booking.jsx
+++ front/src/components/Booking/Booking.jsx
@@ -217,7 +217,7 @@ const Booking = () => {
             onChange={handleChange}
 
             className={styles.formInput2}
-          />  */}
+          />  */} 
           {isFormComplete && formData.totalTime > 0 && formData.totalTime < 9 && (
   <button type="submit" className={styles.bookingButton}>Reservar</button>
 )}
