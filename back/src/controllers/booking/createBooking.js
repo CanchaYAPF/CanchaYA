503de599277890592b2b35ada67a09af8173a421
vapretito <titomanbarreto@gmail.com>
2023-10-26 14:18:35 -0300

    mis reservas, /disponibilidad distintos dias

--- back/src/controllers/booking/createBooking.js
+++ back/src/controllers/booking/createBooking.js
@@ -60,66 +60,87 @@
 
 
 const { Booking, Field } = require('../../db');
-const jwt = require('jsonwebtoken'); // Asegúrate de importar jwt si aún no lo has hecho.
-const { Op } = require('sequelize');// Para consultas complejas
+const jwt = require('jsonwebtoken');
+const { Op } = require('sequelize');
 
 async function createBooking(day, initialHour, finalHour, totalTime, fieldId, userId) {
     try {
-        if (!day || !initialHour || !finalHour || !totalTime || !fieldId || !userId) {
-            throw new Error("Faltan datos por completar");
-        }
-        const decoded = jwt.verify(userId, 'secretKey');
-        console.log("decode", decoded.userId);
-        const UserId = decoded.userId;
-        //verificar si existe field
-      // const field=await Field.findOne({where:{id:fieldId}})
-    
-      const field=await Field.findByPk(fieldId)
-
-       //falta opcion para que busque en la bd harcodeada
-       if(!field) {
-        throw new Error("Esta cancha no existe")}
-        else{
-        //verificar horarios disponibles de ese field
+      if (!day || !initialHour || !finalHour || !totalTime || !fieldId || !userId) {
+        throw new Error("Faltan datos por completar");
+      }
+  
+      const decoded = jwt.verify(userId, 'secretKey');
+      console.log("decode", decoded.userId);
+      const UserId = decoded.userId;
+  
+      const field = await Field.findByPk(fieldId);
+  
+      if (!field) {
+        throw new Error("Esta cancha no existe");
+      } else {
+        // Verificar horarios disponibles de ese field en un rango de días
         const fieldAvailableHours = await field.getBookings({
-            where: {
+          where: {
+            [Op.and]: [
+              {
                 day,
                 [Op.or]: [
-                    { initialHour: {
-                    [Op.between]: [initialHour, finalHour]}},
-                    { finalHour: {
-                    [Op.between]: [initialHour, finalHour]}},
-                    {[Op.and]: [
-                    { initialHour: { [Op.lte]: initialHour }},
-                    { finalHour: { [Op.gte]: finalHour } }]}
-                ]}
-            });
-
-            if(fieldAvailableHours.length === 0){
-                //crea booking y lo asocia con field
-                const booking= await Booking.create({day,initialHour,finalHour,totalTime,UserId,FieldId:fieldId})
-                // await booking.addField(field);
-
-                const bookingWithField = await Booking.findOne({
-                    where: { id: booking.id },
-                    include: {
-                      model: Field,
-                      attributes: ['name'], // Especifica el campo que deseas
-                    },
-                  });
-                  
-                console.log("bookingwithfield",bookingWithField)
-                return bookingWithField
-                
-            }else{
-                throw new Error("Horario no disponible")
-            }
-
+                  {
+                    [Op.and]: [
+                      {
+                        initialHour: {
+                          [Op.between]: [initialHour, finalHour],
+                        },
+                      },
+                      {
+                        finalHour: {
+                          [Op.between]: [initialHour, finalHour],
+                        },
+                      },
+                      {
+                        [Op.and]: [
+                          { initialHour: { [Op.lte]: initialHour } },
+                          { finalHour: { [Op.gte]: finalHour } },
+                        ],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          },
+        });
+  
+        if (fieldAvailableHours.length === 0) {
+          // Crea la reserva y la asocia con el campo
+          const booking = await Booking.create({
+            day,
+            initialHour,
+            finalHour,
+            totalTime,
+            UserId,
+            FieldId: fieldId,
+          });
+  
+          const bookingWithField = await Booking.findOne({
+            where: { id: booking.id },
+            include: {
+              model: Field,
+              attributes: ['name'],
+            },
+          });
+  
+          console.log("bookingwithfield", bookingWithField);
+          return bookingWithField;
+        } else {
+          throw new Error("Horario no disponible");
         }
-
+      }
     } catch (error) {
-        console.error(error);
-        throw new Error(error.message);
+      console.error(error);
+      throw new Error(error.message);
     }
-}
-module.exports = createBooking;
+  }
+  
+  module.exports = createBooking;
+  
