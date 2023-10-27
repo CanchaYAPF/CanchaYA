d64eac3bd58262d350ff96f49518280e50693db8
Angela Barrenechea <angelacba86@gmail.com>
2023-10-27 10:21:28 -0500

    quite nombre

--- back/src/handlers/admin/getFieldsHr.js
+++ back/src/handlers/admin/getFieldsHr.js
@@ -1,9 +1,9 @@
 const getFields = require("../../controllers/admin/getFields")
 
 const getFieldsHr =async(req,res)=>{
-    const {name}= req.body
+    // const {name}= req.body
 try {
-    const response= getFields(name)
+    const response= getFields()
     res.status(200).json(response)
 } catch (error) {
     res.status(400).send({error: error.message})
