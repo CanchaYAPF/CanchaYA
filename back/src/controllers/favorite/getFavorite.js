7c58eeece706f469aa4900a9104bb20e66868db0
unknown <joaquinjavier.naya@gmail.com>
2023-10-30 16:32:27 -0300

    mockkk

--- back/src/controllers/favorite/getFavorite.js
+++ back/src/controllers/favorite/getFavorite.js
@@ -1,41 +1,31 @@
-const { Favorito,Field } = require("../../db");
-const jwt = require("jsonwebtoken")
-const mock = require ("../../../../mockUp")
-
-
+const { Favorito, Field } = require("../../db");
+const jwt = require("jsonwebtoken");
+const mock = require("../../../mockUp");
 
 const getAllFavs = async (id) => {
-  
-  const decoded = jwt.verify(id, 'secretKey');
-  console.log("decode",decoded.userId)
-  const userId = decoded.userId
+  const decoded = jwt.verify(id, "secretKey");
+  console.log("decode", decoded.userId);
+  const userId = decoded.userId;
 
+  const allFavoRelations = await Favorito.findAll({
+    where: { idUser: userId },
+  });
 
-  const allFavoRelations =  await Favorito.findAll({where:{idUser : userId},})
+  // const ids = allFavoRelations.map( fav=>fav.idsFields)
+  // const idsUU = ids.filter( id=> id.length> 10)
 
+  // const resposeDb = idsUU.map(id =>{
 
+  // return  Field.findAll({where:{id : id}})
 
-  
-// const ids = allFavoRelations.map( fav=>fav.idsFields)
-// const idsUU = ids.filter( id=> id.length> 10)
+  // })
 
+  //   const responseApi = allFavoRelations.map( relation => {
+  //     id = relation.idsFields
+  //     return mock.filter(f => f.id===id)
+  //   })
 
-// const resposeDb = idsUU.map(id =>{
-  
-// return  Field.findAll({where:{id : id}})
-
-// })
-
-
-//   const responseApi = allFavoRelations.map( relation => {
-//     id = relation.idsFields
-//     return mock.filter(f => f.id===id)
-//   })
-
-
-
- return allFavoRelations;
-
+  return allFavoRelations;
 };
 
 module.exports = getAllFavs;
