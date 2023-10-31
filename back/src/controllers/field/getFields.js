7895dc05a557f7974e50bfa84e3ddd1c5212ee8f
Angela Barrenechea <angelacba86@gmail.com>
2023-10-31 13:14:35 -0500

    booking front

--- back/src/controllers/field/getFields.js
+++ back/src/controllers/field/getFields.js
@@ -23,40 +23,41 @@ const getAllFields = async (name) => {
     }
   }
 
-  const normalize = (arr) =>
-    arr.map((field) => {
-      const sport = field.Sports?.map((objeto) => objeto.name);
-      const sports = sport?.join(", ");
+  // const normalize = (arr) =>
+  //   arr.map((field) => {
+  //     const sport = field.Sports?.map((objeto) => objeto.name);
+  //     const sports = sport?.join(", ");
 
-      const shif = field.shift?.map((objeto) => objeto);
-      const shifs = shif?.join(", ");
+  //     const shif = field.shift?.map((objeto) => objeto);
+  //     const shifs = shif?.join(", ");
 
-      const pay = field.paymentMethod?.map((objeto) => objeto);
-      const pays = pay?.join(", ");
+  //     const pay = field.paymentMethod?.map((objeto) => objeto);
+  //     const pays = pay?.join(", ");
 
-      const ser = field.service?.map((objeto) => objeto);
-      const servs = ser?.join(", ");
+  //     const ser = field.service?.map((objeto) => objeto);
+  //     const servs = ser?.join(", ");
 
-      return {
-        id: field.id,
-        name: field.name,
-        image: field.image,
-        sports: sports,
-        address: field.address,
-        city: field.city,
-        phone: field.phone,
-        price: field.price,
-        shift: shifs,
-        paymentMethod: pays,
-        service: servs,
-        status:field.status
-      };
-    });
+  //     return {
+  //       id: field.id,
+  //       name: field.name,
+  //       image: field.image,
+  //       sports: sports,
+  //       address: field.address,
+  //       city: field.city,
+  //       phone: field.phone,
+  //       price: field.price,
+  //       shift: shifs,
+  //       paymentMethod: pays,
+  //       service: servs,
+  //       status:field.status
+  //     };
+  //   });
 
-  const mockNormal = normalize(mock);
-  const database = normalize(allFields);
+  // const mockNormal = normalize(mock);
+  // const database = normalize(allFields);
 
-  return [...database, ...mockNormal];
+  // return [...database, ...mockNormal];
+  return allFields
 };
 
 module.exports = getAllFields;
--- front/src/components/Admin/BookingAdmin/BookingCard.jsx
+++ front/src/components/Admin/BookingAdmin/BookingCard.jsx
@@ -1,9 +1,14 @@
 
-const BookingCard = ()=>{
+const BookingCard = (booking)=>{
 
     return(
         <div>
-
+<tr key={booking.id}>
+    <td>{booking.day}</td>
+    <td>{booking.initialHour}</td>
+    <td>{booking.finalHour}</td>
+    <td>{booking.totalTime}</td>
+</tr>
         </div>
     )
 }
