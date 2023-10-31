129da9cdcf9373b6508a3c0f43570c3e6deb0376
Angela Barrenechea <angelacba86@gmail.com>
2023-10-31 12:11:26 -0500

    funciona todo menos sports

--- back/src/controllers/admin/editField.js
+++ back/src/controllers/admin/editField.js
@@ -1,17 +1,19 @@
 const { Field } = require("../../db");
-const editField =async ( id,name, image, phone,sports, address, city, paymentMethod, price, service, shift ) => {
+const editField =async ( id,name,image,sports,phone,address,city,paymentMethod,price,service,shift) => {
     //verifica y solo incluye los elementos que no llegan vacios de body
     let updateData = {};
-    if (name) updateData.name = name;
-    if (image) updateData.image = image;
-    if (sports) updateData.sports = sports;
-    if (phone) updateData.phone = phone;
-    if (address) updateData.address = address;
-    if (city) updateData.city = city;
-    if (paymentMethod) updateData.paymentMethod = paymentMethod;
-    if (price) updateData.price = price;
-    if (service) updateData.service = service;
-    if (shift) updateData.shift = shift;
+        if (name) updateData.name = name;
+        if (image) updateData.image = image;
+        if (sports) updateData.sports = sports;
+        if (phone) updateData.phone = phone;
+        if (address) updateData.address = address;
+        if (city) updateData.city = city;
+        if (paymentMethod) updateData.paymentMethod = paymentMethod;
+        if (price) updateData.price = price;
+        if (service) updateData.service = service;
+        if (shift) updateData.shift = shift;
+
+
 
     const editingField= Field.update(updateData,{where:{id:id}})
 
--- back/src/handlers/admin/editFieldHr.js
+++ back/src/handlers/admin/editFieldHr.js
@@ -3,10 +3,10 @@ const editField = require("../../controllers/admin/editField")
 
 const editFieldHr=async (req,res)=>{
     const {id}=req.params
-    const {name, image, sports, phone, address, city, paymentMethod, price, service, shift}=req.body
+    const {name,image,sports,phone,address,city,paymentMethod,price,service,shift}=req.body
     
     try {
-        const response = editField(id,name, image, sports, phone, address, city, paymentMethod, price, service, shift)
+        const response = editField(id,name,image,sports,phone,address,city,paymentMethod,price,service,shift)
         res.status(200).json(response)
     } catch (error) {
         res.status(400).send({ error: error.message });
