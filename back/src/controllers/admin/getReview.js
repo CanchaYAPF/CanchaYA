d2ff74611deb3656489813c666f00f06f8e02c8c
Angela Barrenechea <angelacba86@gmail.com>
2023-11-01 11:54:41 -0500

    con formato

--- back/src/controllers/admin/getReview.js
+++ back/src/controllers/admin/getReview.js
@@ -1,7 +1,29 @@
-const{Review} =require("../../db")
+const{Review,User,Field} =require("../../db")
 const getReview = async ()=>{
-const allReviews= await Review.findAll()
+const allReviews= await Review.findAll({
+    include:[{
+        model:User,
+        attributes:['mail'],
+        require:true
+    },{
+        model:Field,
+        attributes:['name'],
+        require: true
+    }]
+})
+const reviewsFormat = await 
+    allReviews?.map(review =>{
+        return {
+            id:review.id,
+            rate:review.rate,
+            description:review.description,
+            approved:review.approved,
+            user:review.User.mail,
+            field:review.Field.name
+        }
+    })
 
-return allReviews
+return reviewsFormat
+
 }
 module.exports=getReview
