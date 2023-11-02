77a8dbb84b2865c8c3a28eee64e6c6a8c1b92573
vapretito <titomanbarreto@gmail.com>
2023-11-02 12:52:19 -0300

    login nuevo estilo

--- front/src/components/Auth/Login/Login.jsx
+++ front/src/components/Auth/Login/Login.jsx
@@ -9,6 +9,8 @@ import { GoogleLogin } from '@react-oauth/google';
 import showPwdImg from '../SignUp/show-password.svg';
 import hidePwdImg from '../SignUp/hide-password.svg';
 import {getUserRole} from '../../../Redux/actions/admin_actions'
+import imagen from './canchas-ya-imgane23.png';
+
 
 
 const Login = () => {
@@ -59,14 +61,19 @@ const Login = () => {
  const [isRevealPwd, setIsRevealPwd] = useState(false); 
 
   return (
-    <div className={style.master}>
-      <div className={style.logo}>
+    <div className={style.master} style={{ backgroundImage: `url(${imagen})` }}>  
+          <div className={style.logo}>
         <h1><img src={logo} alt="" /></h1>
       </div>
       <div className={style.texto}>
-      <h1 className={style.top}>ENCUENTRA, RESERVA Y JUEGA:</h1>
+      <h1 className={style.top}>ENCUENTRA, RESERVA Y JUEGA</h1>
       <h1 className={style.blanco}>LA CANCHA IDEAL A SOLO UN CLIC DE DISTANCIA</h1>
-      <h3 className={style.blanco}>Descubre la forma más sencilla y rápida de reservar canchas deportivas. En <span className={style.resaltado}>CANCHAS YA</span> ponemos el deporte al alcance de tus manos . Encuentra, reserva y juega en las mejores instalaciones en segundos. Tu cancha perfecta, tu juego perfecto. ¡Únete a la comunidad deportiva ahora!</h3>
+      <h3 className={style.blanco}>Descubre la forma más sencilla y rápida de reservar canchas deportivas. En 
+      <span className={style.resaltado}> CanchasYA</span> </h3>
+      <h2 className={style.blanco}> Ponemos el deporte al alcance de tus manos</h2>
+      <h2 className={style.blanco}> Encuentra, reserva y juega en las mejores instalaciones en segundos</h2>
+      <h2 className={style.blanco}> Tu cancha perfecta, tu juego perfecto. ¡Únete a la comunidad deportiva ahora!</h2>
+      <h2 className={style.resaltado}> Para ingresar es necesario crear un usuario </h2>      
       </div>
       <br />
       <div className={style.container}>
