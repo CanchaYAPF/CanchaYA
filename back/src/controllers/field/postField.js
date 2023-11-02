1b8d5bf3886cbdd1e4dd44f56e839b815c3e7b69
vapretito <titomanbarreto@gmail.com>
2023-11-01 23:53:27 -0300

    para dupli dupli

--- back/src/controllers/field/postField.js
+++ back/src/controllers/field/postField.js
@@ -7,7 +7,7 @@ const postField = async (name, image, sports, phone, address, city, paymentMetho
     if (existingField) {
       const error = new Error("La cancha ya existe");
       error.status = 409;
-      throw error;
+      throw error
     }else {
       const sportsToAdd = await Sport.findAll({ where: { name: sports } });
       //Dependiendo de si es googleToken o Token lo decodifica y le da el valor del userId
