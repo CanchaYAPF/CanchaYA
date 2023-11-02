a51ac49666553569f288b130397a8a6768ddcb54
tategrodz <tategrodz2003@gmail.com>
2023-11-01 23:29:49 -0300

    edit fields andando bien

--- front/src/components/Admin/EditFieldForm/FormContent.jsx
+++ front/src/components/Admin/EditFieldForm/FormContent.jsx
@@ -1,17 +1,26 @@
 import styles from './FormContent.module.css'; 
 
-const FormContent = ({ formData, handleChange, handleShiftChange, handleServiceChange, handleSubmit }) => {
+const FormContent = ({ formData, handleInputChange, handleSportChange, handleShiftChange, handleServiceChange, handleSubmit, sports }) => {
   return (
     <div className={styles.modalContent}>
       <form onSubmit={handleSubmit}>
         <label className={styles.formLabel}>
           Nombre:
-          <input type="text" name="name" value={formData.name} onChange={handleChange} className={styles.formInput} />
+          <input type="text" name="name" value={formData.name} onChange={handleInputChange} className={styles.formInput} />
         </label>
-        <label className={styles.formLabel}>
-          Deportes:
-          <input type="text" name="sports" value={formData.sports} onChange={handleChange} className={styles.formInput} />
-        </label>
+        <div className={styles.formLabel}>Deportes:</div>
+        {sports.map((sport, index) => (
+          <div key={index}>
+            <input
+              type="checkbox"
+              name={sport}
+              value={sport}
+              checked={!!formData.sports[sport]}
+              onChange={handleSportChange}
+            />
+            {sport}
+          </div>
+        ))}
         <label className={styles.formLabel}>
           Servicios:
           <input type="checkbox" name="estacionamiento" value="Estacionamiento" checked={Array.isArray(formData.service) && formData.service.includes('Estacionamiento')} onChange={handleServiceChange} />
@@ -36,15 +45,15 @@ const FormContent = ({ formData, handleChange, handleShiftChange, handleServiceC
         </label>
         <label className={styles.formLabel}>
           Dirección:
-          <input type="text" name="address" value={formData.address} onChange={handleChange} className={styles.formInput} />
+          <input type="text" name="address" value={formData.address} onChange={handleInputChange} className={styles.formInput} />
         </label>
         <label className={styles.formLabel}>
           Teléfono:
-          <input type="text" name="phone" value={formData.phone} onChange={handleChange} className={styles.formInput} />
+          <input type="text" name="phone" value={formData.phone} onChange={handleInputChange} className={styles.formInput} />
         </label>
         <label className={styles.formLabel}>
           Precio por hora:
-          <input type="text" name="price" value={formData.price} onChange={handleChange} className={styles.formInput} />
+          <input type="text" name="price" value={formData.price} onChange={handleInputChange} className={styles.formInput} />
         </label>
         <button type="submit" className={styles.editFieldButton}>Actualizar cancha</button>
       </form>
